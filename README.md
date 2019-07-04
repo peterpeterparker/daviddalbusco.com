a7f88deba40543d3951b0f4cb791205a4e9b23d0
peterpeterparker <david.dalbusco@outlook.com>
2019-07-04 18:08:04 +0200

    feat: initial import

---
+++ README.md
@@ -0,0 +1,38 @@
+# DavidDalBusco.com
+
+Hello,
+
+I'm [David Dal Busco]. I'm a freelancer, full stack developer Web, PWA and Mobile iOS/Android, project manager, UX and IT consultant.
+
+This project is my personal website which showcases my portfolio and blog entries.
+
+It is developed with [Gatsby].
+
+## Table of contents
+
+- [Getting Started](#getting-started)
+- [License](#license)
+
+## Getting Started
+
+To develop and run this website locally, proceed as following:
+
+```
+git clone https://github.com/peterpeterparker/daviddalbusco.com
+cd daviddalbusco.com
+npm install
+npm run start
+```
+
+If you don't have the [Gatsby] Cli installed globally on your machine, install it:
+
+```
+npm install -g gatsby-cli
+```
+
+## License
+
+MIT © [David Dal Busco](mailto:david.dalbusco@outlook.com)
+
+[David Dal Busco]: https://daviddalbusco.com
+[Gatsby]: https://www.gatsbyjs.org/
---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `David Dal Busco`,
+    description: `Freelancer | Full Stack Development Web, PWA and Mobile iOS/Android | Project management | UX and IT consulting`,
+    author: `David Dal Busco`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `daviddalbusco.com`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#221f39`,
+        theme_color: `#221f39`,
+        display: `minimal-ui`,
+        icon: `src/images/daviddalbusco-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}

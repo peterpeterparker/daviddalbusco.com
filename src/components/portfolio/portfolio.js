cdaced57e9cd6069032f900248e9288ffa03f506
peterpeterparker <david.dalbusco@outlook.com>
2019-07-05 12:33:32 +0200

    refactor: react component

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,13 +1,37 @@
 import React from "react"
 
 import "./portfolio.scss"
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql, StaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-const Portfolio = () => {
-  const data = useStaticQuery(graphql`
+class Portfolio extends React.Component {
+
+  render() {
+    return <section>
+      <div className="container">
+        <Img fluid={this.props.data.placeholderImage.childImageSharp.fluid}/>
+
+        <h1>David Dal Busco</h1>
+
+        <div className="divider">
+          <div></div>
+
+          <FontAwesomeIcon icon={["fal", "robot"]} size="2x"/>
+
+          <div></div>
+        </div>
+
+        <p>{this.props.data.site.siteMetadata.description}</p>
+      </div>
+    </section>
+  }
+}
+
+export default () => (
+  <StaticQuery
+    query={graphql`
     query {
       placeholderImage: file(relativePath: { eq: "daviddalbusco.jpg" }) {
         childImageSharp {
@@ -22,25 +46,9 @@ const Portfolio = () => {
         }
       }
     }
-  `)
-
-  return <section>
-    <div className="container">
-      <Img fluid={data.placeholderImage.childImageSharp.fluid}/>
-
-      <h1>David Dal Busco</h1>
-
-      <div className="divider">
-        <div></div>
-
-        <FontAwesomeIcon icon={['fal', 'robot']} size="2x" />
-
-        <div></div>
-      </div>
-
-      <p>{data.site.siteMetadata.description}</p>
-    </div>
-  </section>
-}
-
-export default Portfolio
+  `}
+    render={(data) => (
+      <Portfolio data={data}/>
+    )}
+  />
+)

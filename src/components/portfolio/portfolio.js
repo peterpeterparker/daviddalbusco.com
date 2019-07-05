14243ab72f93577ea9cf642545d36d4ac8a71072
peterpeterparker <david.dalbusco@outlook.com>
2019-07-05 16:30:12 +0200

    feat: add applications to portfolio

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -4,6 +4,7 @@ import { graphql, StaticQuery } from "gatsby"
 import "./portfolio.scss"
 
 import Chapter from "../chapter/chapter"
+import Img from "gatsby-image"
 
 class Portfolio extends React.Component {
 
@@ -13,6 +14,44 @@ class Portfolio extends React.Component {
         <Chapter icon="suitcase">
           <h2>Portfolio</h2>
         </Chapter>
+
+        <div className="projects">
+          <article  className="ourenergy">
+            <div className="summary">
+              <Img fluid={this.props.data.ourEnergyImage.childImageSharp.fluid}/>
+            </div>
+
+            <div className="details">
+              <h2>Our Energy - ETH Zürich</h2>
+
+              <p>Details</p>
+            </div>
+          </article>
+
+          <article className="deckdeckgo">
+            <div className="summary">
+              <Img fluid={this.props.data.deckDeckGoImage.childImageSharp.fluid}/>
+            </div>
+
+            <div className="details">
+              <h2>DeckDeckGo</h2>
+
+              <p>Details</p>
+            </div>
+          </article>
+
+          <article className="fluster">
+            <div className="summary">
+              <Img fluid={this.props.data.flusterImage.childImageSharp.fluid}/>
+            </div>
+
+            <div className="details">
+              <h2>Fluster</h2>
+
+              <p>Details</p>
+            </div>
+          </article>
+        </div>
       </main>
     </section>
   }
@@ -22,9 +61,23 @@ export default () => (
   <StaticQuery
     query={graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "daviddalbusco.jpg" }) {
+      ourEnergyImage: file(relativePath: { eq: "portfolio/ourenergy-icon.png" }) {
         childImageSharp {
-          fluid(maxWidth: 480) {
+          fluid(maxWidth: 240) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      },
+      deckDeckGoImage: file(relativePath: { eq: "portfolio/deckdeckgo-icon.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 240) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      },
+      flusterImage: file(relativePath: { eq: "portfolio/fluster-icon.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 240) {
             ...GatsbyImageSharpFluid
           }
         }

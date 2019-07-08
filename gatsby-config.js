0d376aaed04fb008063ea31cc769ef089027f22f
peterpeterparker <david.dalbusco@outlook.com>
2019-07-08 21:10:35 +0200

    feat: activate service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,9 +35,7 @@ module.exports = {
         icon: `src/images/daviddalbusco-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     `gatsby-plugin-sass`
   ],
 }

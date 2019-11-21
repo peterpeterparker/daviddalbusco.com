d81a34ab614d270eee4fd1f7819aadbd3960bce6
peterpeterparker <david.dalbusco@outlook.com>
2019-11-21 12:43:15 +0100

    feat: update deckdeckgo showcase

--- src/pages/portfolio/deckdeckgo.js
+++ src/pages/portfolio/deckdeckgo.js
@@ -7,25 +7,25 @@ import Chapter from "../../components/chapter/chapter"
 import PortFolio from "../../components/portfolio/portfolio"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
+import { debounce } from "@deckdeckgo/utils"
+
 class DeckDeckGoPage extends React.Component {
 
   async componentDidMount() {
-    // TODO: Workaround. To be removed when https://github.com/ionic-team/stencil/issues/1724 will be solved
-    try {
-      const deckdeckgoCoreLoader = require("@deckdeckgo/core/dist/loader")
+    this.initFrameSize()
 
-      await deckdeckgoCoreLoader.defineCustomElements(window)
-    } catch (err) {
-      console.error(err);
-    }
+    window.addEventListener("resize", debounce(() => {
+      this.initFrameSize()
+    }))
+  }
 
-    const youtube = document.querySelector('deckgo-youtube');
+  initFrameSize() {
+    const frameElement = document.querySelector("iframe")
 
-    if (youtube) {
-      youtube.width = youtube.parentElement.offsetWidth > 768 ? youtube.parentElement.offsetWidth / 2 : youtube.parentElement.offsetWidth;
-      youtube.height= youtube.parentElement.offsetHeight;
-
-      await youtube.lazyLoadContent();
+    if (frameElement) {
+      const width = frameElement.parentElement.offsetWidth > 560 ? frameElement.parentElement.offsetWidth / 2 : frameElement.parentElement.offsetWidth
+      frameElement.width = `${width}`
+      frameElement.height = `${(width * 3) / 4}`
     }
   }
 
@@ -41,14 +41,18 @@ class DeckDeckGoPage extends React.Component {
 
           <article className="info">
             <div>
-              <p>I started this project, "DeckDeckGo", in october 2018. I had to prepare a talk about <a href="https://ionicframework.com">Ionic</a> v4 and Web Components and I noticed that I wasn't using the technology I was about to display.</p>
-              <p>As any engineer, what did I do in such situation? I created quickly a library using <a href="https://stenciljs.com">StencilJS</a> to write my presentation only with Web Components.</p>
-              <p>After the presentation I did continue to "work" on my spare time on the project as it is for me a way to learn new technologies and concept.</p>
-              <p>Meanwhile <a href="https://nmattia.com">Nicolas Mattia</a> joined the project and together we decided to have even more fun by turning it into a fully web open source editor for presentations.</p>
+              <p>I started this project, "DeckDeckGo", in october 2018. I had to prepare a talk about <a
+                href="https://ionicframework.com">Ionic</a> v4 and Web Components and I noticed that I wasn't using the
+                technology I was about to display.</p>
+              <p>As any engineer, what did I do in such situation? I created quickly a library using <a
+                href="https://stenciljs.com">StencilJS</a> to write my presentation only with Web Components.</p>
+              <p>After the presentation I did continue to "work" on my spare time on the project as it is for me a way
+                to learn new technologies and concept.</p>
+              <p>Meanwhile <a href="https://nmattia.com">Nicolas Mattia</a> joined the project and together we decided
+                to have even more fun by turning it into a fully web open source editor for presentations.</p>
             </div>
 
-            <deckgo-youtube src="https://www.youtube.com/watch?v=3o3oGBTTRSs" frameTitle={'DeckDeckGo'}>
-            </deckgo-youtube>
+            <iframe frameBorder={0} src="https://beta.deckdeckgo.io/daviddalbusco/introducing-deckdeckgo/"></iframe>
           </article>
         </main>
       </section>
@@ -62,17 +66,24 @@ class DeckDeckGoPage extends React.Component {
           <article>
             <div>
               <p><strong>Available:</strong> Progressive Web Apps</p>
-              <p><strong>Technology:</strong> <a href="https://stenciljs.com">StencilJS</a>, <a href="http://ionicframework.com">Ionic</a>, <a href="https://webrtc.org">WebRTC</a>, <a href="https://d3js.org">D3js</a>, etc.</p>
               <p><strong>Website:</strong> <a href="https://deckdeckgo.com"> DeckDeckGo.com</a></p>
             </div>
             <div>
-              <p><strong>Documentation:</strong> <a href="https://docs.deckdeckgo.com"> Docs.DeckDeckGo.com</a></p>
+              <p><strong>Technology:</strong> <a href="https://stenciljs.com">StencilJS</a>, <a
+                href="http://ionicframework.com">Ionic</a>, <a href="https://webrtc.org">WebRTC</a>, <a
+                href="https://d3js.org">D3js</a>, etc.</p>
               <p><strong>Infrastructure:</strong> <a href="https://firebase.google.com">Google Cloud Firebase</a></p>
-              <p><strong>Database:</strong> <a href="https://firebase.google.com/docs/firestore">Google Firestore</a></p>
+              <p><strong>Database:</strong> <a href="https://firebase.google.com/docs/firestore">Google Firestore</a>
+              </p>
             </div>
           </article>
 
-          <p>The complexity of the project goes way beyond what is summarized above as each presentations written with DeckDeckGo are packaged and published online as self autonomous Progressive Web Apps. For such process we notably use <a href="https://aws.amazon.com/">Amazon AWS</a>, <a href="https://www.haskell.org/">Haskell</a>, <a href="https://nixos.org/nix/">Nix</a> and <a href="https://www.terraform.io/">Terraform</a>. All the credits for the implementation of this amazing tool chain goes to <a href="https://nmattia.com">Nicolas Mattia</a>.</p>
+          <p>The complexity of the project goes way beyond what is summarized above as each presentations written with
+            DeckDeckGo are packaged and published online as self autonomous Progressive Web Apps. For such process we
+            notably use <a href="https://aws.amazon.com/">Amazon AWS</a>, <a
+              href="https://www.haskell.org/">Haskell</a>, <a href="https://nixos.org/nix/">Nix</a> and <a
+              href="https://www.terraform.io/">Terraform</a>. All the credits for the implementation of this amazing
+            tool chain goes to <a href="https://nmattia.com">Nicolas Mattia</a>.</p>
         </main>
       </section>
 
@@ -83,13 +94,14 @@ class DeckDeckGoPage extends React.Component {
           </Chapter>
 
           <article>
-            <p>This project is open source and available on <a href="https://github.com/deckgo/deckdeckgo">Github<FontAwesomeIcon
+            <p>This project is open source and available on <a
+              href="https://github.com/deckgo/deckdeckgo">Github<FontAwesomeIcon
               icon={["fab", "github"]}/></a></p>
           </article>
         </main>
       </section>
 
-      <PortFolio filter={'deckdeckgo'}/>
+      <PortFolio filter={"deckdeckgo"}/>
     </Layout>
   }
 

94c4b60217561b22e356ba4ec728691496ea1bd1
peterpeterparker <david.dalbusco@outlook.com>
2019-11-21 12:52:29 +0100

    feat: update about

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -13,8 +13,9 @@ class About extends React.Component {
         </Chapter>
 
         <p>I'm a freelancer, full stack developer specialized in <mark>Web</mark>, <mark>Progressive Web Apps</mark> and <mark>Mobile iOS/Android</mark> projects. I've got experience in project management and team lead. I also provide UX and IT consulting.</p>
-        <p>You could hire my services to work closely with your team or ask me to take over your overall project as I did with the projects I showcase in my portfolio.</p>
+        <p>In terms of development, my favorite languages, which I'm experienced with, are <mark><a href="https://stenciljs.com">StencilJS</a></mark>, <mark><a href="http://ionicframework.com">Ionic</a></mark>, <mark><a href="http://angular.io">Angular</a></mark> and <mark><a href="https://www.java.com">Java</a></mark>.</p>
         <p>I speak fluently french, german and english and I'm currently based in Zürich, Switzerland.</p>
+        <p>You could hire me as a contractor to work closely with your team or ask me to take over your overall project as I did with most of the projects I showcase in my portfolio.</p>
       </main>
     </section>
   }

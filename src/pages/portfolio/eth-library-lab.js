f2d927afdda9d27041951fa18431872f28beee15
peterpeterparker <david.dalbusco@outlook.com>
2019-12-02 10:58:57 +0100

    feat: add ETH Library Lab page and description

---
+++ src/pages/portfolio/eth-library-lab.js
@@ -0,0 +1,75 @@
+import React from "react"
+import SEO from "../../components/seo/seo"
+import Layout from "../../components/layout/layout"
+import { graphql, StaticQuery } from "gatsby"
+
+import Chapter from "../../components/chapter/chapter"
+import PortFolio from "../../components/portfolio/portfolio"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+class EthLibraryLabPage extends React.Component {
+
+  render() {
+    return <Layout fixNav={true}>
+      <SEO title="ETH Library Lab"/>
+
+      <section className="project extrabigspace">
+        <main>
+          <Chapter img={this.props.data.ethLibraryLab.childImageSharp.fluid}>
+            <h1>ETH Library Lab</h1>
+          </Chapter>
+
+          <article className="info">
+            <div>
+              <p>I am providing consulting services to the <a href="http://www.librarylab.ethz.ch">ETH Library Lab</a>, an initiative for human-centered innovation in the knowledge sphere from <a href="https://ethz.ch/">ETH Zürich</a>.</p>
+              <p>The lab has for mission to help students, researchers and educators to unleash their full potential by boosting ideas that support them in discovering, accessing, using or sharing scientific information and knowledge.</p>
+              <p>As part of their projects, I use my engineering background and experience to provide:</p>
+              <ul>
+                <li>advices on project management, planning and services</li>
+                <li>discuss technical questions</li>
+                <li>recommandations on design and UX topics</li>
+                <li>support regarding the open source and prototyping concepts</li>
+              </ul>
+            </div>
+          </article>
+        </main>
+      </section>
+
+      <section className="open-source">
+        <main>
+          <Chapter icon="book-spells">
+            <h2>Open source</h2>
+          </Chapter>
+
+          <article>
+            <p>The Lab seeks to advance tools, systems, practices and services and therefore will publish, at least, some projects and prototypes as open source softwares on <a
+              href="https://github.com/eth-library-lab">Github<FontAwesomeIcon
+              icon={["fab", "github"]}/></a></p>
+          </article>
+        </main>
+      </section>
+
+      <PortFolio filter={"eth-library-lab"}/>
+    </Layout>
+  }
+
+}
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        ethLibraryLab: file(relativePath: { eq: "portfolio/eth-library-lab-icon.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 240) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={(data) => (
+      <EthLibraryLabPage data={data}/>
+    )}
+  />
+)

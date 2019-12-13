bce0fb82c8f220e1c5df4cd65779cfe86e8271d8
peterpeterparker <david.dalbusco@outlook.com>
2019-12-13 07:24:17 +0100

    feat: create robots.txt
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,7 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-sitemap`
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`
   ],
 }

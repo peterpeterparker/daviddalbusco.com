df62e7c547ff27c7ae40f787cae38ac01d3b05fc
peterpeterparker <david.dalbusco@outlook.com>
2020-08-15 13:26:44 +0200

    feat: review text
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `David Dal Busco`,
-    description: `Freelancer - Web, PWA and Mobile iOS/Android Development - Project management - UX and IT consulting`,
+    description: `Freelance Web Developer - Web, Progressive Web Apps and Mobile - Project management - UX and IT consulting`,
     author: `David Dal Busco`,
     url: `https://daviddalbusco.com`,
     image: "/images/daviddalbusco.jpg",
--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,7 +1,10 @@
 import React from "react"
+
 import Chapter from "../chapter/chapter"
 
 import "./about.scss"
+
+import { Link } from "gatsby"
 
 class About extends React.Component {
 
@@ -12,11 +15,13 @@ class About extends React.Component {
           <h2>About</h2>
         </Chapter>
 
-        <p>I'm a freelance web developer specialized in Web, Progressive Web Apps and Mobile (iOS/Android) projects.</p>
-        <p>In terms of development, I'm experienced with <mark><a href="http://ionicframework.com">Ionic</a></mark>, <mark><a href="http://angular.io">Angular</a></mark> and newly <mark><a href="https://reactjs.org">React</a></mark>. I'm a huge fan of Web Components developed with <mark><a href="https://stenciljs.com">StencilJS</a></mark> and I have a <mark><a href="https://www.java.com">Java</a></mark> background, which probably explains why I like to use <mark><a href="https://www.typescriptlang.org">Typescript</a></mark>.</p>
-        <p>I also have got experience in project management, business requirements analysis, used to be a team lead and provide UX and IT consulting.</p>
+        <p>Hey, nice to meet you 👋.</p>
+        <p>I'm a freelance web developer. I am experienced with <mark><a href="http://ionicframework.com">Ionic</a></mark>, <mark><a href="http://angular.io">Angular</a></mark>, <mark><a href="https://reactjs.org">React</a></mark> and I'm a huge fan of <mark><a href="https://stenciljs.com">StencilJS</a></mark> Web Components.</p>
+        <p>After my engineering degree, I spent many years collaborating on Java projects, which probably explains why I like to use <mark><a href="https://www.typescriptlang.org">Typescript</a></mark> and why I can understand backend developers 😉.</p>
+        <p>I also have got experience in project management, analysis of business requirements and I used to be a team lead. I also provide UX and IT consulting.</p>
         <p>I speak fluently french, german and english and I'm currently based in Zürich, Switzerland.</p>
-        <p>You could hire me as a contractor to be part of your team or ask me to take over your overall project, as I did with some of the projects I showcase in my portfolio.</p>
+        <p>You could hire me as a contractor, to be part of your team, or ask me to take over your overall projects.</p>
+        <p>If you have got a Web, Progressive Web Apps or mobile (iOS/Android) projects in mind, <mark><Link to="/#contact">get in touch!</Link></mark></p>
       </main>
     </section>
   }

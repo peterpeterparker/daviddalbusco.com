d89d5260852880ed504ab0c23ce7ff839d3084d5
peterpeterparker <david.dalbusco@outlook.com>
2022-02-22 08:19:21 +0100

    chore: double twitter:creator
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- src/lib/components/seo.svelte
+++ src/lib/components/seo.svelte
@@ -20,7 +20,6 @@
 <meta content={url ?? defaultUrl} property="og:url" />
 <meta content={image ?? defaultImage} property="og:image" />
 <meta content="summary_large_image" name="twitter:card" />
-<meta content={title ?? defaultTitle} name="twitter:creator" />
 <meta content={title ?? defaultTitle} name="twitter:title" />
 <meta content={description ?? defaultDescription} name="twitter:description" />
 <meta content={image ?? defaultImage} name="twitter:image" />

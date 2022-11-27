fa616d333020d8f9129456022b5ab19967f6cfce
David Dal Busco <david.dalbusco@outlook.com>
2022-11-27 20:34:25 +0100

    build: bump sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -8,11 +8,7 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter(),
-    prerender: {
-      default: true
-    },
-    trailingSlash: 'always'
+    adapter: adapter()
   }
 };
 

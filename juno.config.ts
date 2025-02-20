7cf3bb5d38aefb808eec1bd6b2649c1d76d7b831
David Dal Busco <david.dalbusco@outlook.com>
2025-02-20 12:44:22 +0100

    feat: config hosting HTTP headers for static content

--- juno.config.ts
+++ juno.config.ts
@@ -1,9 +1,25 @@
-import {defineConfig} from '@junobuild/config';
+import { defineConfig } from '@junobuild/config';
 
 export default defineConfig({
-  satellite: {
-    id: 'w43uv-5qaaa-aaaal-ar6ta-cai',
-    source: 'build',
-    predeploy: ['npm run build']
-  }
+	satellite: {
+		id: 'w43uv-5qaaa-aaaal-ar6ta-cai',
+		source: 'build',
+		predeploy: ['npm run build'],
+		storage: {
+			headers: [
+				{
+					source: 'icons/**/*',
+					headers: [['Cache-Control', 'max-age=31536000']]
+				},
+				{
+					source: 'images/**/*',
+					headers: [['Cache-Control', 'max-age=31536000']]
+				},
+				{
+					source: 'fonts/**/*',
+					headers: [['Cache-Control', 'max-age=31536000']]
+				}
+			]
+		}
+	}
 });

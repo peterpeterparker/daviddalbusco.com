fad8179b63042fc50a64a1a3bc194b5beea0a734
David Dal Busco <david.dalbusco@outlook.com>
2025-02-20 12:58:11 +0100

    build: use version to setup build reproducibility (#99)

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const { version } = JSON.parse(json);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		version: {
+			name: version
+		}
 	}
 };
 

2f3c089d31eba90c9382f478faa7752d2e6ea3aa
David Dal Busco <david.dalbusco@outlook.com>
2026-04-17 16:21:33 +0200

    chore: modern juno.config (#165)

--- juno.config.ts
+++ juno.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from '@junobuild/config';
 
 export default defineConfig({
 	satellite: {
-		id: 'w43uv-5qaaa-aaaal-ar6ta-cai',
-		source: 'build',
-		predeploy: ['pnpm run build'],
+		ids: {
+			production: 'w43uv-5qaaa-aaaal-ar6ta-cai'
+		},
+		hosting: {
+			source: 'build',
+			predeploy: ['pnpm run build'],
+		},
 		storage: {
 			headers: [
 				{

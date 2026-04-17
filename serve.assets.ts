8a5a054dc3678d0999db2b901e3028e3301d2ad6
David Dal Busco <david.dalbusco@outlook.com>
2026-04-17 16:19:30 +0200

    feat: better 404 and cleanup (#164)

--- serve.assets.ts
+++ serve.assets.ts
@@ -1,15 +1,19 @@
+import { join } from 'node:path';
+
 const BASE_PATH = './assets';
+const PORT = 3000;
 
 Bun.serve({
-	port: 3000,
-	async fetch(req) {
-		const filePath = BASE_PATH + new URL(req.url).pathname;
+	port: PORT,
+	async fetch({ url }) {
+		const { pathname } = new URL(url);
+		const filePath = join(BASE_PATH, pathname);
 		const file = Bun.file(filePath);
 		return new Response(file);
 	},
-	error() {
-		return new Response(null, { status: 404 });
+	error(error) {
+		return new Response(`Error: ${error.message}`, { status: 404 });
 	}
 });
 
-console.log(`Assets served from ${BASE_PATH} on http://localhost:3000`);
+console.log(`Assets served from ${BASE_PATH} on http://localhost:${PORT}`);

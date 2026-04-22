96af9a372622dac35512944cc7252bb1a3d7caec
David Dal Busco <david.dalbusco@outlook.com>
2026-04-22 08:25:08 +0200

    feat: update social image

--- src/lib/components/Seo.svelte
+++ src/lib/components/Seo.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	let defaultUrl = 'https://daviddalbusco.com';
-	let defaultImage = `${defaultUrl}/images/social-image-v3.jpg`;
+	let defaultImage = `${defaultUrl}/images/social-image-v4.jpg`;
 	let defaultTitle = 'David Dal Busco';
 	let defaultDescription = 'Freelance Web Engineer';
 

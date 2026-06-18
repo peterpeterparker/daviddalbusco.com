1a650cbe67c20d6c0fcc18bbdd43f7605f40c8e8
David Dal Busco <david.dalbusco@outlook.com>
2026-06-18 12:29:14 +0200

    feat: prioritize blog post title semantically

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import '../theme/fonts.scss';
 	import '../theme/theme.scss';
 	import '../theme/main.scss';
@@ -11,15 +11,18 @@
 	import { initAnalytics } from '$lib/services/analytics.services.js';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import { page } from '$app/state';
 
 	let { children } = $props();
 
 	onMount(() => {
 		initAnalytics();
 	});
+
+	let tag = $derived<'h1' | 'h3'>(page.route.id === '/blog/[slug]' ? 'h3' : 'h1');
 </script>
 
-<Header />
+<Header {tag} />
 
 <main>
 	{@render children?.()}

dcf8f15660bc4dad067e56dadd57cd8eb3c47bcc
David Dal Busco <david.dalbusco@outlook.com>
2026-06-29 14:54:17 +0200

    feat: remove Juno (#247)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import juno from '@junobuild/vite-plugin';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), juno()]
+	plugins: [sveltekit()]
 });

3a18271b23185fce2f24f8999869c7b09177fbb9
David Dal Busco <david.dalbusco@outlook.com>
2026-06-30 08:39:50 +0200

    feat: headers for assets (#255)

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,35 @@
 import type { ExportedHandler } from 'kyushu-types';
+import { extname } from 'node:path';
+
+const ADDITIONAL_HEADERS: Record<string, [string, ...string[]]> = {
+	'.png': ['public', 'max-age=31536000', 'immutable'],
+	'.webp': ['public', 'max-age=31536000', 'immutable'],
+	'.jpg': ['public', 'max-age=31536000', 'immutable']
+};
+
+const fetchAsset: ExportedHandler['fetch'] = async (request, env) => {
+	const responseFromMemory = await env.ASSETS.fetch(request);
+
+	if (responseFromMemory.status !== 404) {
+		return responseFromMemory;
+	}
+
+	return await env.ASSETS.fetch(request, { src: 'fs' });
+};
 
 export default {
 	async fetch(request, env) {
-		const responseFromMemory = await env.ASSETS.fetch(request);
+		const { headers, ...rest } = await fetchAsset(request, env);
 
-		if (responseFromMemory.status !== 404) {
-			return responseFromMemory;
-		}
+		const { pathname } = URL.parse(request.url) ?? { pathname: undefined };
+		const cache = pathname !== undefined ? ADDITIONAL_HEADERS[extname(pathname)] : undefined;
 
-		return await env.ASSETS.fetch(request, { src: 'fs' });
+		return {
+			...rest,
+			headers: {
+				...headers,
+				...(cache !== undefined && { 'cache-control': cache.join(', ') })
+			}
+		};
 	}
 } satisfies ExportedHandler;

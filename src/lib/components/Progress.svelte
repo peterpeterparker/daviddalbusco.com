916da9b12dab715d4564ebfb0ed9a3c668a128a6
David Dal Busco <david.dalbusco@outlook.com>
2026-07-02 09:16:31 +0200

    feat: progress bar for blog (#261)

---
+++ src/lib/components/Progress.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	interface Props {
+		anchor: HTMLElement;
+	}
+
+	let { anchor }: Props = $props();
+
+	let value = $state(0);
+
+	const onscroll = () => {
+		const delta = document.documentElement.scrollHeight - anchor.offsetTop;
+		const scrollLength = anchor.offsetTop - delta;
+
+		const y = (window.scrollY * 100) / scrollLength;
+		value = Math.min(100, Math.floor(y));
+	};
+
+	$effect(() => {
+		window.addEventListener('scroll', onscroll, { passive: true });
+
+		return () => {
+			// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener#matching_event_listeners_for_removal
+			window.removeEventListener('scroll', onscroll, false);
+		};
+	});
+</script>
+
+<progress {value} max="100"></progress>
+
+<style lang="scss">
+	progress {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 0.25rem;
+
+		z-index: 10;
+
+		appearance: none;
+		border: none;
+	}
+
+	// Color: https://stackoverflow.com/a/76860350/5404186
+	progress {
+		color: var(--color-highlight);
+		background: transparent;
+
+		/* Firefox: Filled portion of the progress bar */
+		&::-moz-progress-bar {
+			background: currentColor;
+		}
+
+		/* Chrome & Safari: Unfilled portion of the progress bar */
+		&::-webkit-progress-bar {
+			background: transparent;
+		}
+
+		/* Chrome & Safari: Filled portion of the progress bar */
+		&::-webkit-progress-value {
+			background: currentColor;
+		}
+	}
+</style>

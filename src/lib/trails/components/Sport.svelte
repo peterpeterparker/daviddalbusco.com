7738829f5933b62c6b6c68a7b59caa10b3fd0814
David Dal Busco <david.dalbusco@outlook.com>
2026-07-06 18:44:09 +0200

    feat: components to show trail details (#293)

---
+++ src/lib/trails/components/Sport.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Sport } from '$lib/trails/types/trail';
+
+	interface Props {
+		sport: Sport;
+	}
+
+	let { sport }: Props = $props();
+</script>
+
+<mark>
+	{#if sport === 'trail-running'}
+		Trail Running
+	{:else if sport === 'gravel'}
+		Gravel Bike
+	{:else}
+		Road Bike
+	{/if}
+</mark>

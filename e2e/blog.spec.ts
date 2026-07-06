4d4612037cee277373d532331227e3f8e52eb2a8
David Dal Busco <david.dalbusco@outlook.com>
2026-07-06 09:10:15 +0200

    test: wait async img

--- e2e/blog.spec.ts
+++ e2e/blog.spec.ts
@@ -5,6 +5,13 @@ test('match screenshot', async ({ page }) => {
 
 	await expect(page.getByText('Making Your Website AI-Ready')).toBeVisible();
 
+	// https://github.com/microsoft/playwright/issues/6046#issuecomment-3641164427
+	await page.waitForFunction(() => {
+		return Array.from(document.querySelectorAll('img')).every(
+			(img) => img.complete && img.naturalWidth > 0
+		);
+	});
+
 	await expect(page).toHaveScreenshot({
 		fullPage: true,
 		maxDiffPixelRatio: 0.1

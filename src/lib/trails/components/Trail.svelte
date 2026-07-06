bd3225900cc0a04118b742b22032d46f80123d8f
David Dal Busco <david.dalbusco@outlook.com>
2026-07-06 18:41:39 +0200

    feat: components to show the list of trails

---
+++ src/lib/trails/components/Trail.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import type { PageDataWithoutContent } from '$lib/types/page';
+	import Card from '$lib/components/Card.svelte';
+	import type { Trail } from '$lib/trails/types/trail';
+	import { formatDate } from '$lib/utils/date.utils';
+	import Sport from '$lib/trails/components/Sport.svelte';
+	import { assetUrl } from '$lib/utils/assets.utils';
+
+	interface Props {
+		trail: PageDataWithoutContent<Trail>;
+	}
+
+	let { trail }: Props = $props();
+
+	let metadata = $derived(trail.metadata);
+	let slug = $derived(trail.slug);
+
+	let title = $derived(metadata.metadata.title);
+	let image = $derived(assetUrl(metadata.metadata.photos[0]));
+	let trailDate = $derived(metadata.metadata.date);
+	let sport = $derived(metadata.metadata.sport);
+</script>
+
+<Card background="transparent" cover={true} {image} path="trails" {slug}>
+	<h3>{title}</h3>
+
+	<p class="sport"><Sport {sport} /></p>
+	<p class="date">{formatDate(trailDate)}</p>
+</Card>
+
+<style lang="scss">
+	h3 {
+		font-size: 1rem;
+		margin: 0 0 0.45rem;
+	}
+
+	.date,
+	.sport {
+		font-size: 0.8rem;
+		margin: 0;
+	}
+</style>

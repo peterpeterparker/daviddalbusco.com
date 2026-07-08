1f5e9753aa620c47e815e6478a658c65e9e6f7aa
David Dal Busco <david.dalbusco@outlook.com>
2026-07-08 17:32:31 +0200

    feat: revert

--- src/lib/trails/constants.ts
+++ src/lib/trails/constants.ts
@@ -2,5 +2,4 @@ import { SITE_URL } from '$lib/core/constants';
 
 export const TRAILS_SOCIAL_IMAGE = `${SITE_URL}/images/trails-social-image-v1.jpg`;
 export const TRAILS_TITLE = 'Trails, David Dal Busco';
-export const TRAILS_DESCRIPTION =
-	'Running and cycling adventures in the Swiss Alps and beyond, with GPS tracks, elevation profiles, and photos from every trail';
+export const TRAILS_DESCRIPTION = 'Running and cycling adventures in the Swiss Alps and beyond';

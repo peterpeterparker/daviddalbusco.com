9e34239bf047a5c757c1a590439dff237e9d4ba4
David Dal Busco <david.dalbusco@outlook.com>
2026-07-08 14:03:39 +0200

    feat: trails seo and social image (#331)

---
+++ src/lib/core/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_URL = 'https://daviddalbusco.com';
+export const SITE_SOCIAL_IMAGE = `${SITE_URL}/images/social-image-v6.jpg`;
+export const SITE_TITLE = 'David Dal Busco';
+export const SITE_DESCRIPTION = 'Freelance Software Engineer';
--- src/routes/trails/+page.svelte
+++ src/routes/trails/+page.svelte
@@ -5,6 +5,7 @@
 	import type { PageData as ServerPageData } from './$types';
 	import type { Trail } from '$lib/trails/types/trail';
 	import Breadcrumb from '$lib/core/components/Breadcrumb.svelte';
+	import { TRAILS_DESCRIPTION, TRAILS_SOCIAL_IMAGE, TRAILS_TITLE } from '$lib/trails/constants';
 
 	interface Props {
 		data: ServerPageData;
@@ -15,7 +16,7 @@
 </script>
 
 <svelte:head>
-	<Seo />
+	<Seo image={TRAILS_SOCIAL_IMAGE} title={TRAILS_TITLE} description={TRAILS_DESCRIPTION} />
 
 	<style lang="scss">
 		@use '../../theme/_page.scss';

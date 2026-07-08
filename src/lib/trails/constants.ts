8372785329966077fac177c5e36c24e6042f0a3e
David Dal Busco <david.dalbusco@outlook.com>
2026-07-08 17:28:42 +0200

    feat: linkedin requires 100 chars for the og description

--- src/lib/trails/constants.ts
+++ src/lib/trails/constants.ts
@@ -2,4 +2,5 @@ import { SITE_URL } from '$lib/core/constants';
 
 export const TRAILS_SOCIAL_IMAGE = `${SITE_URL}/images/trails-social-image-v1.jpg`;
 export const TRAILS_TITLE = 'Trails, David Dal Busco';
-export const TRAILS_DESCRIPTION = 'Running and cycling adventures in the Swiss Alps and beyond';
+export const TRAILS_DESCRIPTION =
+	'Running and cycling adventures in the Swiss Alps and beyond, with GPS tracks, elevation profiles, and photos from every trail';

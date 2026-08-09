187f7cab70b00b262b2984c195d5a5fa8feb0feb
David Dal Busco <david.dalbusco@outlook.com>
2026-08-09 19:41:24 +0200

    feat: better SEO for portfolio and blog (#359)

---
+++ src/lib/portfolio/constants.ts
@@ -0,0 +1,3 @@
+export const PORTFOLIO_TITLE = 'Portfolio: Freelance Software Engineering Projects';
+export const PORTFOLIO_DESCRIPTION =
+	'A collection of freelance software engineering projects, from Swiss insurance and blockchain to civic tech and research tools for ETH Zürich.';

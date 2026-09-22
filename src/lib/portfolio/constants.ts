181bedcb45d65452a0da88bd988dade6c05c41f1
David Dal Busco <david.dalbusco@outlook.com>
2026-09-22 20:54:16 +0200

    feat: json ld for portfolio and trails

--- src/lib/portfolio/constants.ts
+++ src/lib/portfolio/constants.ts
@@ -1,3 +1,7 @@
+import { SITE_URL } from '$lib/core/constants';
+
 export const PORTFOLIO_TITLE = 'Portfolio: Freelance Software Engineering Projects';
 export const PORTFOLIO_DESCRIPTION =
 	'A collection of freelance software engineering projects, from Swiss insurance and blockchain to civic tech and research tools for ETH Zürich.';
+
+export const PORTFOLIO_URL = `${SITE_URL}/portfolio`;
---
+++ src/lib/portfolio/linked-data.ts
@@ -0,0 +1,34 @@
+import { PORTFOLIO_DESCRIPTION, PORTFOLIO_TITLE, PORTFOLIO_URL } from '$lib/portfolio/constants';
+import type { PortfolioMetadata } from '$lib/portfolio/types/portfolio';
+import { AUTHOR_URL, SITE_URL } from '$lib/core/constants';
+import { AUTHOR_LINKED_DATA } from '$lib/core/linked-data';
+import type { Article, CollectionPage } from 'schema-dts';
+
+export const PORTFOLIO_LINKED_DATA: CollectionPage = {
+	'@type': 'CollectionPage',
+	'@id': PORTFOLIO_URL,
+	name: PORTFOLIO_TITLE,
+	description: PORTFOLIO_DESCRIPTION,
+	url: PORTFOLIO_URL,
+	isPartOf: {
+		'@type': 'WebSite',
+		'@id': SITE_URL
+	},
+	author: { '@id': AUTHOR_URL }
+};
+
+export const portfolioToLinkedData = ({
+	title,
+	description,
+	canonical,
+}: PortfolioMetadata & { canonical: string }): Article => ({
+	'@type': 'Article',
+	'@id': canonical,
+	headline: title,
+	description,
+	url: canonical,
+	inLanguage: 'en',
+	mainEntityOfPage: { '@type': 'WebPage', '@id': canonical },
+	isPartOf: { '@id': PORTFOLIO_URL },
+	author: AUTHOR_LINKED_DATA
+});
